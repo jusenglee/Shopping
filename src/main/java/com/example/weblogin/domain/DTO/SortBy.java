2fe0fc9e9231ce3f75f85eb5a38e91cb9b7b6595
dltmdwn00 <dltmdwn00@naver.com>
2023-06-20 19:34:19 +0900

    상품 상세보기 수정 중 0620

--- src/main/java/com/example/weblogin/domain/DTO/SortBy.java
+++ src/main/java/com/example/weblogin/domain/DTO/SortBy.java
@@ -1,6 +1,8 @@
 package com.example.weblogin.domain.DTO;
 
 public enum SortBy {
+
+    //검색 조건을 위해 enum 작성
     PRICE_ASC,  // 가격 오름차순
     PRICE_DESC, // 가격 내림차순
     POPULARITY, // 인기도 (조회수) 내림차순
